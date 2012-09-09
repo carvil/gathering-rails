beeadd49e984f5a3ce5c199cc6f1c11e848d60e3
Jeremy Crosen <jeremy.crosen@gmail.com>
2012-09-08 20:41:31 -0700

    Initial commit with rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GatheringRails::Application.config.secret_token = 'a9be44f3f174f3a169f545bc7000ab91e80c22a42f6cee769d1639af76dbb12f0f0c6f77499eba9d2d55ae707aa397b0f8828f8085187a4a9be2a3be9f26e6e4'
